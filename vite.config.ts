490718c4abd2e1a972ef6bb75f7b1714e0986da0
KristinaRiabova304 <kriabova@kse.org.ua>
2024-11-24 14:55:30 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,14 @@ export default defineConfig({
   },
   test: {
     environment: "jsdom",
-    coverage: {
-      reporter: ["text", "json", "json-summary"],
-    },
+     coverage: {
+  reporter: ["text", "json", "json-summary"],
+  lines: 100,
+  branches: 100,
+  functions: 100,
+  statements: 100,
+},
+
     setupFiles: ["./test/setup.ts"],
   },
 });
